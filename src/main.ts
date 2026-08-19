daa07fc76b1bdbacb5970f2ea82e23f000a70e3b
sszobaer <ahmedsszobaer@gmail.com>
2026-08-19 12:42:38 +0600

    chore: initialize NestJS backend with Prisma and Scalar

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Project & Task Management API')
+    .setDescription(
+      'RESTful API for managing projects and tasks with authentication and authorization.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.use(
+    '/api/docs',
+    apiReference({
+      content: document,
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 4000);
+}
+
+bootstrap();
